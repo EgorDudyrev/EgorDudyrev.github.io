a792fbea32e9da6c87e89b24bb9b88d387f251f0
Egor Dudyrev <egor.dudyrev@loria.fr>
2023-10-22 18:09:39 +0200

    Put custom CSS to main.css

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>    
+    <img alt="Portait" class="logo" src="@/assets/portrait_big.jpg" width="200"/>
+    <div>    
+        <h1>Egor Dudyrev</h1>
+        <h2>PhD student in explainable rule-based machine learning</h2>
+        <p>
+            I am a 2nd year PhD student in <a href="https://www.univ-lorraine.fr">Université de Lorraine</a> (Nancy, France) and <a href="https://www.hse.ru/">HSE University</a> (Moscow, Russia).
+            I am working full-time as a doctorant in <a href="https://www.loria.fr/">Loria</a> research laboratory in Nancy, France (team <a href="https://orpailleur.loria.fr">Orpailleur</a>).
+            My main research interests lie in applying the mathematical language of Formal Concept Analysis (FCA) to practical problems of Explainable rule-based machine learning and Ordered data analysis.
+        </p>
+        <p class="color_main_light"><i>&lt;The website is under construction&gt;</i></p>
+    </div>
+</template>
