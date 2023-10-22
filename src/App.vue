1ad0f06211eb040aa475e04146b45f902235141c
Egor Dudyrev <egor.dudyrev@loria.fr>
2023-10-22 21:36:34 +0200

    Add some style

--- src/App.vue
+++ src/App.vue
@@ -4,39 +4,15 @@
 import TheHeader from './components/TheHeader.vue'
 </script>
 
+
 <template>
   <TheHeader></TheHeader>
   
   <!--<RouterView />-->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script>
+  export default {
+    name: 'App'
   }
-
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+</script>
