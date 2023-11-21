9d34eab8474ebac268b03371eb146a7dd36b5fa4
Egor Dudyrev <egor.dudyrev@loria.fr>
2023-11-21 23:53:32 +0100

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env sh
 
-# abort on errors
-set -e
-
-# build
 npm run build
-
-# navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:egordudyrev/egordudyrev.github.io.git main
-
-cd -
+git add -f dist && git commit -m 'update dist subtree'
+git subtree push --prefix dist origin gh-pages
