f0ef90e85df92cde1803af680813b61954faa58c
Egor Dudyrev <egor.dudyrev@loria.fr>
2024-08-04 16:58:58 +0200

    Add boilerplate hardcoded body text

--- src/components/TheBody.vue
+++ src/components/TheBody.vue
@@ -4,12 +4,115 @@
 
 <template>
 <body>
+  <section>
+    <h1>Talks</h1>
 
+    <h2>Casual Concept Analysis</h2>
+    Where: PhD Pizza time @ LORIA / INRIA, Nancy, France
+    When: Jun 10, 2024
+    Language: EN
+    Abstract: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    Links: <a>Slides</a> <a>LinkedIn post</a>
+    Published: Aug 4, 2024
+
+
+    <h2>Неформальное введение в Анализ Формальных Понятий</h2>
+    Where: NoML Seminar @ Online
+    Language: RU
+    Abstract: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    Links: <a>Slides</a> <a>YouTube recording</a>
+
+  </section>
+  <section>
+    <h1>Software</h1>
+
+    <h2>Caspailleur</h2>
+    Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    Links: <a>GitHub</a> <a>PyPI</a>
+
+    <h2>Paspailleur</h2>
+    Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    Links: <a>GitHub</a> <a>PyPI</a>
+
+    <h2>Expailleur</h2>
+    Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    Links: <a>GitHub</a>
+
+  </section>
+  <section>
+    <h1>Teaching</h1>
+
+    <h2>Data Mining</h2>
+    Where: TD at IDMC, Nancy, France
+    When: September - November 2023
+
+    <h2>Ordered Sets in Data Analysis</h2>
+    Where: Seminars at HSE University, Moscow, Russia
+    When: September - October 2022
+
+  </section>
+  <section>
+    <h1>Publications</h1>
+
+    <h2>Data Complexity: An FCA-based approach</h2>
+    Where: International Journal of Approximate Reasoning 165, 109084
+    When: February 2024
+    Authors: A Buzmakov, E Dudyrev, SO Kuznetsov, T Makhalova, A Napoli
+    Abstract: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+  </section>
+  <section>
+    <h1>Education</h1>
+
+    <h2>Doctoral Study (WIP)</h2>
+    Where: Université de Lorraine
+    When: January 2023 - ...
+
+    <h2>Doctoral Study (WIP)</h2>
+    Where: HSE University
+    When: November 2021 - ...
+
+    <h2>Master</h2>
+    Where: HSE University
+    When: September 2019 - June 2021
+
+    <h2>Bachelor</h2>
+    Where: Bauman University
+    When: September 2015 - June 2019
+
+  </section>
+  <section>
+    <h1>Work</h1>
+
+    <h2>Doctorant</h2>
+    Where: Team Orpailleur @ LORIA, Nancy, France
+    When: December 2022 - November 2025
+
+    <h2>Intern Researcher</h2>
+    Where: ISSA Laboratory @ HSE University, Moscow, Russia
+    When: April 2021 - November 2022
+
+    <h2>Data Scientist</h2>
+    Where: DWH Department @ Sportmaster, Moscow, Russia
+    When: November 2018 - April 2021
+
+    <h2>Data Science Intern</h2>
+    Where: Treasury Department @ Sberbank, Moscow, Russia
+    When: April 2018 - November 2018
+  </section>
 </body>
 </template>
 
 <style scoped>
+h1 {
+  text-align: left;
+}
 body {
+  margin-left:auto;
+  margin-right:auto;
+  margin-top:2em;
 
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
 }
 </style>
