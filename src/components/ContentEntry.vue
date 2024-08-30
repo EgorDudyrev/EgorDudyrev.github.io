8bffb559b90a7b8f5f03eda6af70af56300d8604
Egor Dudyrev <egor.dudyrev@loria.fr>
2024-08-30 21:32:29 +0200

    Move "published" note on the same line with location, date, etc

--- src/components/ContentEntry.vue
+++ src/components/ContentEntry.vue
@@ -17,12 +17,18 @@ const props = defineProps({
 <template>
   <div>
     <h2>{{ title }}</h2>
+
     <ul v-show="props.authors" class="inline_list">Authors: <li v-for="author in props.authors"> {{ author.name }}</li></ul>
-    <ul class="inline_list">
-      <li><IconText icon-name="location-dot" :text="props.location"></IconText></li>
-      <li><IconText icon-name="calendar-alt" :text="props.date"></IconText></li>
-      <li><IconText icon-name="language" :text="props.language"></IconText></li>
-    </ul>
+
+    <div style="display: flex; justify-content: space-between">
+      <ul class="inline_list">
+        <li><IconText icon-name="location-dot" :text="props.location"></IconText></li>
+        <li><IconText icon-name="calendar-alt" :text="props.date"></IconText></li>
+        <li><IconText icon-name="language" :text="props.language"></IconText></li>
+      </ul>
+      <i class="color_comment_light">Published: {{ props.publicationDate }}</i>
+    </div>
+
     <ul v-show="props.links" class="inline_list">
       <li v-for="link in props.links"><IconText :url="link.url" :icon-name="link.faicon" :text="link.title"></IconText></li>
     </ul>
@@ -31,7 +37,7 @@ const props = defineProps({
       <div class="abstract">{{ props.abstract }}</div>
       <br>
     </details>
-    <p class="color_comment_light"><i>Published: {{ props.publicationDate }}</i></p>
+    <!--<p class="color_comment_light"><i>Published: {{ props.publicationDate }}</i></p>-->
   </div>
 </template>
 
